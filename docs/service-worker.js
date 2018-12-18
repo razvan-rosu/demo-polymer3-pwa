217e7d1373d60b61aec65751c97d0fce5f154487
Razvan Rosu <razvan-rosu89@gmail.com>
2018-12-19 01:02:46 +0200

    Update build app

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","ef62797d01a055d678fb793c58190003"]];
+var precacheConfig = [["index.html","a4ec36ef405a22188f798d83ef91ec0e"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
