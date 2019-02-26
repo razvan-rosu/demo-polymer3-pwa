bf6dea82d5b61c2d48615923e710c218bf162bd8
Razvan Rosu <razvan-rosu89@gmail.com>
2019-02-26 09:57:54 +0200

    Update demo

--- docs/sw.js
+++ docs/sw.js
@@ -8,6 +8,58 @@ workboxSW.router.registerRoute(/.*(?:typicode)\.com.*$/, workboxSW.strategies.st
 
 workboxSW.precache([
   {
+    "url": "assets/android-launchericon-144-144.png",
+    "revision": "8697491bb7241f4880488f5de158a37b"
+  },
+  {
+    "url": "assets/android-launchericon-192-192.png",
+    "revision": "29177a86d889319a5ddca395b50c0f4e"
+  },
+  {
+    "url": "assets/android-launchericon-48-48.png",
+    "revision": "61a8f49c1df316eb15e3b3082389ce9b"
+  },
+  {
+    "url": "assets/android-launchericon-512-512.png",
+    "revision": "3bc92c93aa38c543e24118ccbbcb7961"
+  },
+  {
+    "url": "assets/android-launchericon-72-72.png",
+    "revision": "7142a00a266f0e5b0435c3ae25edff8f"
+  },
+  {
+    "url": "assets/android-launchericon-96-96.png",
+    "revision": "ccc6ea4a88ab341607f0dda5562f0a87"
+  },
+  {
+    "url": "assets/chrome-favicon-16-16.png",
+    "revision": "7c7b77e934852c4abc87d399f6b2021f"
+  },
+  {
+    "url": "assets/chrome-installprocess-128-128.png",
+    "revision": "fb6de2efc8a7d127e2a50bd2d2bfd7c4"
+  },
+  {
+    "url": "assets/ios-appicon-120-120.png",
+    "revision": "99e4118486d7b90658204df25fe5015a"
+  },
+  {
+    "url": "assets/ios-appicon-152-152.png",
+    "revision": "1f4162c9af7b489a022f291ab0c1c3e7"
+  },
+  {
+    "url": "assets/ios-appicon-180-180.png",
+    "revision": "5b320749d205a147d235ccf9ddb5345d"
+  },
+  {
+    "url": "assets/ios-appicon-76-76.png",
+    "revision": "51815fa63688ac20a447869edfe8e99e"
+  },
+  {
+    "url": "assets/logo.png",
+    "revision": "a4580736c54749a39a58624d8114dce3"
+  },
+  {
     "url": "index.html",
     "revision": "09f5a420b9d562326aa337519a03b112"
   },
@@ -73,7 +125,7 @@ workboxSW.precache([
   },
   {
     "url": "sw.js",
-    "revision": "a85459513c641d58e821835cc6661e42"
+    "revision": "26e7760a577aa7a249084e8da72c730f"
   },
   {
     "url": "workbox-sw.prod.v2.1.3.js",
